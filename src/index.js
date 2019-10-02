330ae53e033af38bfb56e9de8fb899be15997846
Vladislav Shevel <vladislav.shevel@gmail.com>
2019-10-02 22:15:23 +0300

    fix:modify index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  var reversea = first.split('').reverse();
+  var reverseb = second.split('').reverse();
+
+  var result = [];
+
+  for (var i = 0; i < reversea.length; i++) {
+    for (var j = 0; j < reverseb.length; j++) {
+      var m = reversea[i] * reverseb[j];
+      result[i + j] = (result[i + j]) ? result[i + j] + m : m;
+    }
+  }
+
+  for (var i = 0; i < result.length; i++) {
+    var modulo = result[i] % 10;
+    var ten = Math.floor(result[i] / 10);
+    result[i] = modulo;
+    
+    if (!result[i + 1] && ten == 0)
+      break;
+    if (result[i + 1] && ten)
+      result[i + 1] += ten;
+    else if (ten)
+      result[i + 1] = ten;   
+  }
+ return result.reverse().join('');
 }
